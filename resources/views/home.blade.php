5c09300f23bff9bf842a85dbff6846f281774ac7
Osagie Erhabor <erhaborosagie@gmail.com>
2020-06-28 09:30:49 +0100

    Front page design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img class="rounded-circle" src="https://instagram.flos6-1.fna.fbcdn.net/v/t51.2885-19/s150x150/43129914_2173458166199646_3827222400865402880_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_ohc=m1Z9WpIANXYAX9qoZBF&oh=7e5a0f2acc97b1e143fc5f2a2af84a99&oe=5F213F62">
+        </div>
+        <div class="col-9">
+            <div>
+                <h1>osagie.erhabor</h1>
+            </div>
+            <div class="pt-5 d-flex">
+                <div class="pr-5"><strong>5</strong> posts</div>
+                <div class="pr-5"><a href="#"><strong>82</strong> followers</a></div>
+                <div class="pr-5"><a href="#"><strong>129</strong> following</a></div>
+            </div>
+            <div class="pt-5">
+                <div style="font-weight: bold">Osagie Erhabor</div>
+                <div>Summary</div>
+                <div><a href="#">Link</a></div>
             </div>
         </div>
+
+    </div>
+    <div class="row pt-5">
+        <div class="col-4 p-3"><a href="#"><img class="w-100" src="https://instagram.flos6-1.fna.fbcdn.net/v/t51.2885-15/e35/93503118_2991019367621649_1458516803024093087_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=wSswpr94gbMAX-eHycv&oh=bb07cfac934869d3ce17229c0f153eb1&oe=5F215DB2"></a></div>
+        <div class="col-4 p-3"><a href="#"><img class="w-100" src="https://instagram.flos6-1.fna.fbcdn.net/v/t51.2885-15/e35/93503118_2991019367621649_1458516803024093087_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=wSswpr94gbMAX-eHycv&oh=bb07cfac934869d3ce17229c0f153eb1&oe=5F215DB2"></a></div>
+        <div class="col-4 p-3"><a href="#"><img class="w-100" src="https://instagram.flos6-1.fna.fbcdn.net/v/t51.2885-15/e35/93503118_2991019367621649_1458516803024093087_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=wSswpr94gbMAX-eHycv&oh=bb07cfac934869d3ce17229c0f153eb1&oe=5F215DB2"></a></div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pr-3"><img src="/svg/logo.svg" style="height: 20px; "></div>
+                    <div class="pl-3" style="border-left: 1px solid #123">{{ config('app.name', 'freeCodeGram') }}</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
